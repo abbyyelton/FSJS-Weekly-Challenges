0b8d135a85ff57b90b12e5051946879465b02b2a
Abby Yelton <abbyyelton@gmail.com>
2016-09-05 15:36:25 -0400

    Compeleted Week 2 Challenge

--- Challenges/Week2/app.js
+++ Challenges/Week2/app.js
@@ -1,8 +1,16 @@
-//create function
-  //control and return statements
+//function that takes user response and determines reply
+function parseResponse( response ) {
+  var reply;
+  if ( response === "hello" ) {
+    reply = "Hello World!";
+  } else {
+    reply = "Goodbye!";
+  }
+  return reply;
+}
 
 //Prompt user for input and store variable
+var userResponse = prompt( "Hello!" );
 
-//Call function
-
-//Alert user results
+//Call function and alert user results
+alert ( parseResponse( userResponse.toLowerCase() ) );
