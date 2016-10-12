fa36233c33d18bad47f63f0dd4df058945411139
Abby Yelton <abbyyelton@Abbys-MacBook-Pro-2.local>
2016-10-11 22:39:34 -0400

    Completed Week7 Challenge

--- Challenges/Week7/app.js
+++ Challenges/Week7/app.js
@@ -1,1 +1,34 @@
 //See readme for challenge instructions
+var getRepos = require('./github.js');
+
+//get username(s) from command line
+var args = process.argv.slice(2);
+args.forEach( function(username) {
+  //get Repos
+  var repo = new getRepos(username);
+
+  //Write repos
+  repo.on('end', function(repos) {
+    console.log('Username:');
+    console.log(repos[0].owner.login);
+    console.log('Repositories:');
+    repos.forEach( function(item) {
+      var consoleString = item.name;
+      //check for description
+      if (item.description != '') {
+        consoleString += ' -- ' + item.description;
+      }
+      //see if forked
+      if (item.fork) {
+        consoleString += ' -- Forked';
+      }
+      console.log(consoleString);
+    });
+  });
+
+  repo.on('error', function(error) {
+    //show error
+    console.log('There was an error: ' + error.message);
+  });
+
+});
--- Challenges/Week7/github.js
+++ Challenges/Week7/github.js
@@ -2,7 +2,7 @@
 
 /*
  * Note about github api: requires User-Agent header to be set. This can be done
- * in Node by passing an options object (rather than a simple url string) as the 
+ * in Node by passing an options object (rather than a simple url string) as the
  * first param to the https.get() function
  *
  * So something like
@@ -16,6 +16,60 @@
  *
  */
 
-function getRepos(username){
-  console.log('Repos for ' + username);
+var EventEmitter = require("events").EventEmitter;
+var https = require("https");
+var http = require("http");
+var util = require("util");
+
+function getRepos(username) {
+
+    EventEmitter.call(this);
+
+    var reposEmitter = this;
+
+    var options = {
+       hostname: 'api.github.com',
+       path: '/users/' + username + '/repos',
+       headers: {
+         'User-Agent': 'abbyyelton'
+       }
+     };
+
+    //Connect to the API URL
+    var request = https.get(options, function(response) {
+        var body = "";
+
+        if (response.statusCode !== 200) {
+            request.abort();
+            //Status Code Error
+            reposEmitter.emit("error", new Error("There was an error getting the repos for " + username + ". (" + http.STATUS_CODES[response.statusCode] + ")"));
+        }
+
+        //Read the data
+        response.on('data', function (chunk) {
+            body += chunk;
+            reposEmitter.emit("data", chunk);
+        });
+
+        response.on('end', function () {
+            if(response.statusCode === 200) {
+                try {
+                    //Parse the data
+                    var repos = JSON.parse(body);
+                    reposEmitter.emit("end", repos);
+                } catch (error) {
+                    reposEmitter.emit("error", error);
+                    console.log("there was an error");
+                }
+            }
+        }).on("error", function(error){
+            reposEmitter.emit("error", error);
+            console.log("there was an error");
+        });
+    });
+
 }
+
+util.inherits( getRepos, EventEmitter );
+
+module.exports = getRepos;
